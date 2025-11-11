2d0538991a9c8833842fae0aea6a4343674c76e9
Erik Ramos <mgmtderik@gmail.com>
2025-11-11 02:02:53 -0300

    Initial Commit

---
+++ tictactoe.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+char arr[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+void showBoard();
+int checkForWin();
+
+int main()
+{
+    int player = 1, choice, i;
+    char mark; // X or O
+
+    do 
+    {
+        showBoard();
+        player = (player % 2) ? 1 : 2;
+
+        printf("Player %d, enter a number: ", player);
+        scanf("%d", &choice);
+
+        mark = (player == 1) ? 'X' : 'O';
+
+        if (choice == 1 && arr[1] == '1')
+            arr[1] = mark;
+        else if (choice == 2 && arr[2] == '2')
+            arr[2] = mark;
+        else if (choice == 3 && arr[3] == '3')
+            arr[3] = mark;
+        else if (choice == 4 && arr[4] == '4')
+            arr[4] = mark;
+        else if (choice == 5 && arr[5] == '5')
+            arr[5] = mark;
+        else if (choice == 6 && arr[6] == '6')
+            arr[6] = mark;
+        else if (choice == 7 && arr[7] == '7')
+            arr[7] = mark;
+        else if (choice == 8 && arr[8] == '8')
+            arr[8] = mark;
+        else if (choice == 9 && arr[9] == '9')
+            arr[9] = mark;
+        else
+        {
+            printf("Invalid move\n");
+            player--;
+        }
+
+        i = checkForWin();
+        player++;
+    } while (i == -1);
+
+    showBoard();
+    if (i == 1)
+        printf("==> Player %d won!\n", --player);
+    else
+        printf("==> Game draw!\n");
+
+    return 0;
+}
+
+void showBoard()
+{
+    printf("\n\tTIC TAC TOE\n\n");
+    printf("       |       |     \n");
+    printf("    %c  |   %c   |  %c \n", arr[1], arr[2], arr[3]);
+    printf("-------|-------|-------\n");
+    printf("    %c  |   %c   |  %c \n", arr[4], arr[5], arr[6]);
+    printf("-------|-------|-------\n");
+    printf("    %c  |   %c   |  %c \n\n", arr[7], arr[8], arr[9]);
+}
+
+int checkForWin()
+{
+    // Rows
+    if (arr[1] == arr[2] && arr[2] == arr[3]) return 1;
+    else if (arr[4] == arr[5] && arr[5] == arr[6]) return 1;
+    else if (arr[7] == arr[8] && arr[8] == arr[9]) return 1;
+
+    // Columns
+    else if (arr[1] == arr[4] && arr[4] == arr[7]) return 1;
+    else if (arr[2] == arr[5] && arr[5] == arr[8]) return 1;
+    else if (arr[3] == arr[6] && arr[6] == arr[9]) return 1;
+
+    // Diagonals
+    else if (arr[1] == arr[5] && arr[5] == arr[9]) return 1;
+    else if (arr[3] == arr[5] && arr[5] == arr[7]) return 1;
+
+    // Draw
+    else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' &&
+             arr[4] != '4' && arr[5] != '5' && arr[6] != '6' &&
+             arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+        return 0;
+
+    else
+        return -1;  // Game still going
+}
